9ca1cfb6fe3e5c43d249749abdf4bfc66e1faf52
J0eCool <count.j0ecool@gmail.com>
2015-05-16 18:38:32 -0700

    Updates for gh-pages, push test version

--- jengine/Point.hx
+++
@@ -1,6 +0,0 @@
-package jengine;
-
-typedef Point = {
-    var x :Int;
-    var y :Int;
-}
--- jengine/util/Log.hx
+++
@@ -1,7 +0,0 @@
-package jengine.util;
-
-class Log {
-    public static function log(message :String) :Void {
-        trace(message);
-    }
-}
